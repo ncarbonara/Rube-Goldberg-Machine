aa344b7af0de399c1e1058c09a17b137eb7357c0
Unknown <ncarbonara235@gmail.com>
2017-09-24 15:05:00 -0400

    Camera Control and Obstruction Pillar Added
    Heading says all.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform lookAtTarget;	//What we're looking at
+	public Transform moveToTarget;	//Where the camera is positioned
+	
+	// Update is called once per frame
+	void Update () {
+		//Makes sure we're actually looking at something,
+		//code crashes otherwise when there isn't anything to look at
+		if (lookAtTarget != null) {
+			//Makes camera actually look at something
+			transform.LookAt (lookAtTarget.position);
+		}
+
+		if (moveToTarget != null) {
+			//Makes transform move towards something
+
+			//Calc vector from camera to destination
+			Vector3 moveDirection = moveToTarget.position - transform.position;
+
+			//Is moveDirection > 1? If so, normalize it. 
+			//Otherwise, leave it alone so you don't overshoot.
+			if (moveDirection.magnitude > 1) {
+				moveDirection = moveDirection.normalized;
+				//OR, OR, OR:
+				moveDirection = Vector3.Normalize(moveDirection);
+			}
+
+			//Move transform towards its destination
+			transform.position += moveDirection * Time.deltaTime * 10f;
+		}
+	}
+}
---
+++ Assets/Scripts/CameraTriggerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script belongs on a trigger
+public class CameraTriggerScript : MonoBehaviour
+{
+
+	public Transform lookOverride;	//What the trigger sets the main CameraControl to look at
+	public Transform moveOverride;	//What the trigger sets the CameraControl to move to
+
+	void OnTriggerEnter (Collider activator)
+	{
+		Debug.Log ("CameraTrigger " + name + " triggered by " + activator.name);
+
+		//Finding the CameraController component
+		CameraController camControl = Camera.main.GetComponent<CameraController> ();
+
+		//Now, let's override it
+		if (moveOverride != null) {
+			camControl.moveToTarget = moveOverride;
+		}
+
+		if (lookOverride != null) {
+			camControl.lookAtTarget = lookOverride;
+		}
+	}
+
+	//Only happens in the editor (Wow!)
+	void OnDrawGizmos ()
+	{
+		//Draws a line between two things
+		if (moveOverride != null) {
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine (transform.position, moveOverride.position);
+		}
+
+		if (lookOverride != null) {
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine (transform.position, lookOverride.position);
+		}
+
+	}
+}
