74267d961be7a69886fd5806ceccb62399ec3477
Unknown <ncarbonara235@gmail.com>
2017-09-25 14:16:24 -0400

    Ball Dropper Fully Functional!
    Yay!

---
+++ Assets/Scripts/DropperScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropperScript : MonoBehaviour {
+
+	bool drop = false;	//The variable that determines whether or not the ball should be dropped
+	public GameObject UIText;	//The text telling people how to drop the ball
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Allows the player to press space to move the dropper
+		if (Input.GetKey(KeyCode.Space)) {
+			drop = true;
+
+		}
+
+		//Moves the dropper once space has been pressed
+		if (drop == true) {
+			this.transform.Translate (Vector3.left * Time.deltaTime * 50);
+			UIText.SetActive (false);	//Turns off the UI test
+		}
+	}
+}
